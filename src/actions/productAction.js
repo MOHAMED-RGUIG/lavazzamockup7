b34b63514a1c9deb50537f14000ec53d839a4448
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-06-21 14:06:13 +0200

    Update productAction.js

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 export const getAllProducts = () => async dispatch => {
-    dispatch({ type: 'GET_PRODUCTS_REQUEST' })
+    dispatch({ type: 'GET_PRODUCTS_REQUEST' });
 
     try {
-        const response = await axios.get('https://lavazzamockup-api1.onrender.com/api/products/getallproducts');
+        const response = await axios.get('https://lavazzamockup-api1.onrender.com/api/products/getallproducts', {
+            withCredentials: true // Ensure credentials are included if necessary
+        });
         console.log(response);
         dispatch({ type: 'GET_PRODUCTS_SUCCESS', payload: response.data });
     } catch (error) {

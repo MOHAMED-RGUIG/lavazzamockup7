50306e1f979db0b86ffbddac8ca245eb525ff9fd
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-06-21 13:40:36 +0200

    Update orderActions.js

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -23,7 +23,7 @@ export const getUserOrders = () => async (dispatch, getState) => {
     dispatch({ type: 'GET_USER_ORDERS_REQUEST' });
 
     try {
-        const response = await axios.post('https://lavazzamockup-api1.onrender.com/api/orders/getuserorders', { userid: currentUser[0]._id });
+        const response = await axios.post('/api/orders/getuserorders', { userid: currentUser[0]._id });
         dispatch({ type: 'GET_USER_ORDERS_SUCCESS', payload: response.data });
     } catch (error) {
         dispatch({ 
@@ -39,7 +39,7 @@ export const getAllOrders = () => async dispatch => {
     dispatch({ type: 'GET_ORDERS_REQUEST' });
 
     try {
-        const response = await axios.get('https://lavazzamockup-api1.onrender.com/api/orders/getallorders');
+        const response = await axios.get('/api/orders/getallorders');
         dispatch({ type: 'GET_ORDERS_SUCCESS', payload: response.data });
     } catch (error) {
         dispatch({ 

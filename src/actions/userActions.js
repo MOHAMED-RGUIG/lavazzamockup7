08ef240dbef15423f5ae469de751f33231c44e15
MOHAMED-RGUIG <135667611+MOHAMED-RGUIG@users.noreply.github.com>
2024-06-21 15:42:00 +0200

    Update userActions.js

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -4,7 +4,9 @@ export const registerUser=(user) =>async dispatch=>{
 
     dispatch({type:'USER_REGISTER_REQUEST'})
 try{
-    const response = await axios.post('https://lavazzamockup-api1.onrender.com/api/users/register',user)
+    const response = await axios.post('https://lavazzamockup-api1.onrender.com/api/users/register',user, {
+            withCredentials: true // Ensure credentials are included if necessary
+        })
     console.log(response);
     dispatch({type:'USER_REGISTER_SUCCESS'});
       
@@ -19,7 +21,9 @@ export const loginUser=(user) =>async dispatch=>{
 
     dispatch({type:'USER_LOGIN_REQUEST'})
 try{
-    const response = await axios.post('https://lavazzamockup-api1.onrender.com/api/users/login',user)
+    const response = await axios.post('https://lavazzamockup-api1.onrender.com/api/users/login',user, {
+            withCredentials: true // Ensure credentials are included if necessary
+        })
     console.log(response);
       
     dispatch({type:'USER_LOGIN_SUCCESS', payload: response.data})
